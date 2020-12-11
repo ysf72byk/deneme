4758c62cf287692214c2f4e61215ac4ae7a4d989
ysf72byk <71230633+ysf72byk@users.noreply.github.com>
2020-12-11 13:32:56 +0300

    Update s1.py

--- s1.py
+++ s1.py
@@ -1,25 +1,17 @@
-import time
+
 from multiprocessing.dummy import Pool
 
 import requests
-def x():
-	pool = Pool(10) # Creates a pool with ten threads; more threads = more concurrency.
-	                # "pool" is a module attribute; you can be sure there will only
-	                # be one of them in your application
-	                # as modules are cached after initialization.
-	
-	if __name__ == '__main__':
-	    futures = []
-	    for x in range(10):
-	        futures.append(pool.apply_async(requests.get, ['https://siberdeyiz.com/u/onecame33/9394']))
-	    # futures is now a list of 10 futures.
-	    
-	    for future in futures:
-	        print(future.get()) # For each future, wait until the request is
-	                            # finished and then print the response object
 
+pool = Pool(100) # Creates a pool with ten threads; more threads = more concurrency.
+                # "pool" is a module attribute; you can be sure there will only
+                # be one of them in your application
+                # as modules are cached after initialization.
 
-while 1:
-	x()
-	time.sleep(0.001)
-	
+if __name__ == '__main__':
+    futures = []
+    for x in range(100000):
+        futures.append(pool.apply_async(requests.get, ['https://siberdeyiz.com/u/onecame33/9394']))
+    # futures is now a list of 10 futures.
+    for future in futures:
+        print(future.get())
